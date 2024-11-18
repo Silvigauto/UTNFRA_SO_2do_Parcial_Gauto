224f0a5f2725251e886bc4decde2caa5be236c3e
Silvigauto <silvigauto02@gmail.com>
2024-11-18 01:41:30 +0000

    feat:resolucion punto c

---
+++ RTA_Examen_20241117/Punto_C.sh
@@ -0,0 +1,12 @@
+#touch web1-gauto
+#FROM nginx:latest
+
+#COPY index.html /usr/share/nginx/html/index.html
+
+#docker login -u silvigautoo
+#docker build -t silvigautoo/web1-gauto -f web1-gauto .
+#docker push silvigautoo/web1-gauto
+#run.sh
+#!/bin/bash
+#docker run  -d -p 8080:80 silvigautoo/web1-gauto
+#curl localhost:8080
